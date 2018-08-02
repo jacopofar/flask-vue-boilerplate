2301a199fe0adf6891c54d8c534d3ee9f97b3ec8
Jacopo Farina <jacopofar@users.noreply.github.com>
2018-08-02 12:42:37 +0200

    Rmove root state methods, add session module

--- src/store.js
+++ src/store.js
@@ -1,16 +1,11 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import session from '@/store/session';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-
-  },
-  mutations: {
-
-  },
-  actions: {
-
+  modules: {
+    session,
   },
 });
