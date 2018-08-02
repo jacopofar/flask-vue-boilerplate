6dd8b3c4fde1cde671e864198b84d70182f8790f
Jacopo Farina <jacopofar@users.noreply.github.com>
2018-08-02 11:07:52 +0200

    Add fake login session management

--- vf_boilerplate/fake_login.py
+++ vf_boilerplate/fake_login.py
@@ -2,13 +2,39 @@
 
 Used for development ONLY.
 """
-from flask import Flask
+import logging
+import time
 
+from flask import Flask, jsonify, make_response, session
 
 app = Flask(__name__)
+app.secret_key = 'dev'
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s %(name)s%(levelname)s %(message)s',
+)
 
 
 @app.route("/")
 def index():
     """Log in an user, used only in dev mode."""
-    return "You are logged in for development!"
+    session['mail'] = 'test.user@example.com'
+    session['name'] = 'Luther Blissett'
+    # iat is in the session but never used, here is for debugging
+    # for the "real world" you probably want to use PyJWT
+    session['iat'] = int(time.time())
+    return "You are now logged in for development!"
+
+
+@app.route("/whoami")
+def whoami():
+    """Retrieve the user identity from the session."""
+    if 'mail' not in session:
+        logger.info('Session not present, the user is not logged in')
+        return make_response(jsonify(error='not logged in'), 400)
+    logger.info('The user has a valid session')
+    return jsonify(
+        mail=session['mail'],
+        name=session['name'])
